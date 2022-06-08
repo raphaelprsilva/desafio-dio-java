5775ec5a097760fda21314414f362c48af943cc9
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-06-08 15:29:50 -0300

    implements AutenticadorUtil class

---
+++ src/AutenticadorUtil.java
@@ -0,0 +1,11 @@
+public class AutenticadorUtil {
+  private int senha;
+
+  public void setSenha(int senha) {
+    this.senha = senha;
+  }
+
+  public boolean autentica(int senha) {
+    return this.senha == senha;
+  }
+}
