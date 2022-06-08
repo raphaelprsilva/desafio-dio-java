b97113d88cfb9a65d37275424ada2db422f0638b
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2022-06-08 15:26:53 -0300

    implements ContaPoupanca class

---
+++ src/ContaPoupanca.java
@@ -0,0 +1,17 @@
+public class ContaPoupanca extends Conta {
+
+  public ContaPoupanca(int agencia, int numero) {
+    super(agencia, numero);
+  }
+
+  @Override
+  public void deposita(double valor) {
+    super.saldo += valor;
+  }
+
+  @Override
+  public boolean saca(double valor) {
+    double valorASacar = valor + 0.2;
+    return super.saca(valorASacar);
+  }
+}
